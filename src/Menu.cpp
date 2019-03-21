01229e348ec4bd37760481ca2bbf494b7550aa76
Szymon <szymon.janas.95@gmail.com>
2019-03-21 16:45:58 +0100

    added game virtual and menu layout

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,6 +1,134 @@
-#include "menu.h"
+#include "Menu.hpp"
 
 Menu::Menu()
+{}
+Menu::~Menu()
 {
+    delete board;
+    delete game;
+    delete window;
+}
 
+void Menu::turnOnGame(Layer layer)
+{
+    switch (layer)
+    {
+    case Layer::CONSOLE:
+        setConsoleLayer(ConsoleMenu());
+        break;
+    case Layer::GRAPHIC:
+        setGraphicLayer(GraphicMenu());
+        break;
+    }
+
+
+}
+
+void Menu::setGraphicLayer(Level level)
+{
+    delete window;
+    board = new GameManagement(level);
+    game = new GraphicLayer(board);
+    game->start();
+}
+
+void Menu::setConsoleLayer(Level level)
+{
+    board = new GameManagement (level);
+    game = new ConsoleLayer(board);
+    game->start();
+}
+
+void Menu::drawButton(float posX, float posY, std::string sign, sf::RenderWindow *window)
+{
+    sf::RectangleShape rectangle;
+    rectangle.setSize(sf::Vector2f(100, 40));
+    rectangle.setOutlineColor(sf::Color(140, 140, 140));
+    rectangle.setOutlineThickness(2);
+    rectangle.setFillColor(sf::Color(115, 115, 115));
+    rectangle.setPosition(posX, posY);
+    window->draw(rectangle);
+
+    sf::Font font;
+    if(!font.loadFromFile("../fonts/LatoBlack.ttf"))
+        std::cerr << "ERROR! Font Load From File False!" << std::endl;
+    sf::Text text(sign, font, 20);
+    text.setFillColor(sf::Color::Black);
+    text.setPosition(posX+10, posY+5);
+    window->draw(text);
+}
+
+Menu::Button Menu::decodePosition(int posX, int posY)
+{
+    if (posX >= 50 and posX <= 200){
+        if (posY >= 10 and posY <= 50) return Button::EASY;
+        if (posY >= 60 and posY <= 100) return Button::MEDIUM;
+        if (posY >= 110 and posY <= 150) return Button::HARD;
+        if (posY >= 160 and posY <= 200) return Button::EXIT;
+    }
+}
+
+Level Menu::GraphicMenu()
+{
+    int sizeX = 200;
+    int sizeY = 220;
+    window = new sf::RenderWindow(sf::VideoMode(sizeX, sizeY), "SAPER MENU");
+    while (window->isOpen())
+    {
+        sf::Event event;
+        window->clear(sf::Color(160, 160, 160));
+        drawButton(50, 10, "EASY", window);
+        drawButton(50, 60, "MEDIUM", window);
+        drawButton(50, 110, "HARD", window);
+        drawButton(50, 160, "EXIT", window);
+        window->display();
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                exit(1);
+            }
+            if (event.type == sf::Event::MouseButtonPressed and event.mouseButton.button == sf::Mouse::Left)
+            {
+                switch (decodePosition(event.mouseButton.x, event.mouseButton.y))
+                {
+                case Button::EASY:
+                    return Level::EASY;
+                    break;
+                case Button::MEDIUM:
+                    return Level::MEDIUM;
+                    break;
+                case Button::HARD:
+                    return Level::HARD;
+                    break;
+                case Button::EXIT:
+                    exit(1);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+Level Menu::ConsoleMenu()
+{
+    std::cout << std::endl;
+    std::cout << "==========SAPER MENU==========" << std::endl;
+    std::cout << "=     Please select level    =" << std::endl;
+    std::cout << "=         1. EASY            =" << std::endl;
+    std::cout << "=         2. MEDIUM          =" << std::endl;
+    std::cout << "=         3. HARD            =" << std::endl;
+    std::cout << "=                            =" << std::endl;
+    std::cout << "=         0. EXIT            =" << std::endl;
+    std::cout << "==============================" << std::endl;
+    std::cout << "         YOUR CHOICE: "          << std::endl;
+    int i;
+    std::cin >> i;
+    switch(i)
+    {
+    case 1: return Level::EASY; break;
+    case 2: return Level::MEDIUM; break;
+    case 3: return Level::HARD; break;
+    case 0: exit(1); break;
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,37 @@
 #include<time.h>
 #include<SFML/Window.hpp>
 #include<SFML/Graphics.h>
-#include<Menu.h>
+#include "Menu.hpp"
+#include "ConsoleLayer.hpp"
+#include"GraphicLayer.hpp"
+#include"Game.hpp"
 
 using namespace sf;
 using namespace std;
 
 int main()
 {
-
-
-
-    RenderWindow screen(VideoMode(800, 600), "SFML Works!");
-    screen.display();
-
-    while(screen.isOpen())
+    cout << "WELCOME IN SAPER" << endl;
+    cout << "What kind of view do you prefer?" << endl;
+    cout << "1. Console" << endl;
+    cout << "2. Graphic" << endl;
+    cout << "0. EXIT" << endl;
+    int check = 0;
+    bool checker = true;
+    do
     {
-        screen.clear(Color::Green);
+        cin >> check;
+        if (check >= 0 or check < 3) checker = false;
+    } while (checker);
 
-        RectangleShape rectangle;
-        rectangle.setSize(Vector2f(800, 600));
-        rectangle.setOutlineColor(Color::Yellow);
-        rectangle.setFillColor(Color::Yellow);
-        screen.draw(rectangle);
-
-
-
-        CircleShape shape(50);
-
-        // set the shape color to green
-        shape.setFillColor(Color::Red);
-        screen.draw(shape);
-
-
-        for (clock_t fr = clock(); clock() - fr < 250000;  )
-        { cout << "sec: " << clock() - fr << endl;}
-        screen.close();
+    Menu menu;
+    if (check == 1)
+    {
+        menu.turnOnGame(Layer::CONSOLE);
     }
+    if (check == 2)
+    {
+        menu.turnOnGame(Layer::GRAPHIC);
+    }
+    if (check == 0) exit(1);
 }
